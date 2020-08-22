859014d37f70e5b97596843b236a6d6b1cd627b3
TheBeastLT <pauliox@beyond.lt>
2020-08-22 12:45:43 +0200

    populates kitsu video info when cinemeta has no info

--- addon.js
+++ addon.js
@@ -1,4 +1,3 @@
-const needle = require('needle');
 const { addonBuilder } = require('stremio-addon-sdk');
 const genres = require('./static/data/genres');
 const { enrichKitsuMetadata, enrichImdbMetadata, hasImdbMapping } = require('./lib/metadataEnrich');
@@ -9,93 +8,93 @@ const cinemeta = require('./lib/cinemeta_api');
 const CACHE_MAX_AGE = process.env.CACHE_MAX_AGE || 3 * 24 * 60; // 7 days
 
 const manifest = {
-	id: 'community.anime.kitsu',
-	version: '0.0.1',
-	name: 'Anime Kitsu',
-	description: 'Unofficial Kitsu.io anime catalog addon',
-	logo: 'https://i.imgur.com/ANMG9VF.png',
-	background: 'https://i.imgur.com/ym4n96o.png',
-	resources: ['catalog', 'meta'],
-	types: ['movie', 'series'],
-	catalogs: [
-		{
-			id: 'kitsu-anime-list',
-			name: 'Kitsu',
-			type: 'series',
-			extra: [{ name: 'search', isRequired: false }, { name: 'genre' }],
-			genres: Object.values(genres)
-		},
-		{
-			id: 'kitsu-anime-rating',
-			name: 'Kitsu Highest Rated',
-			type: 'series',
-			extra: [{ name: 'genre' }],
-			genres: Object.values(genres)
-		},
-		{
-			id: 'kitsu-anime-popular',
-			name: 'Kitsu Most popular',
-			type: 'series',
-			extra: [{ name: 'genre' }],
-			genres: Object.values(genres)
-		},
-		{
-			id: 'kitsu-anime-trending',
-			name: 'Kitsu Trending',
-			type: 'series'
-		}
-	],
-	idPrefixes: ['kitsu']
+  id: 'community.anime.kitsu',
+  version: '0.0.1',
+  name: 'Anime Kitsu',
+  description: 'Unofficial Kitsu.io anime catalog addon',
+  logo: 'https://i.imgur.com/ANMG9VF.png',
+  background: 'https://i.imgur.com/ym4n96o.png',
+  resources: ['catalog', 'meta'],
+  types: ['movie', 'series'],
+  catalogs: [
+    {
+      id: 'kitsu-anime-list',
+      name: 'Kitsu',
+      type: 'series',
+      extra: [{ name: 'search', isRequired: false }, { name: 'genre' }],
+      genres: Object.values(genres)
+    },
+    {
+      id: 'kitsu-anime-rating',
+      name: 'Kitsu Highest Rated',
+      type: 'series',
+      extra: [{ name: 'genre' }],
+      genres: Object.values(genres)
+    },
+    {
+      id: 'kitsu-anime-popular',
+      name: 'Kitsu Most popular',
+      type: 'series',
+      extra: [{ name: 'genre' }],
+      genres: Object.values(genres)
+    },
+    {
+      id: 'kitsu-anime-trending',
+      name: 'Kitsu Trending',
+      type: 'series'
+    }
+  ],
+  idPrefixes: ['kitsu']
 };
 const builder = new addonBuilder(manifest);
 const sortValue = {
-	'kitsu-anime-list': 'createdAt',
-	'kitsu-anime-rating': '-average_rating',
-	'kitsu-anime-popular': '-user_count'
+  'kitsu-anime-list': 'createdAt',
+  'kitsu-anime-rating': '-average_rating',
+  'kitsu-anime-popular': '-user_count'
 };
 
 builder.defineCatalogHandler((args) => {
-	const skip = args.extra && args.extra.skip || 0;
-	const id = `${args.id}|${args.extra && args.extra.genre || 'All'}|${skip}`;
+  const skip = args.extra && args.extra.skip || 0;
+  const id = `${args.id}|${args.extra && args.extra.genre || 'All'}|${skip}`;
 
-	if (args.extra && args.extra.search) {
-		// no need to cache search results
-		return kitsu.search(args.extra.search)
-				.then((metas) => ({ metas: metas, cacheMaxAge: CACHE_MAX_AGE }));
-	}
+  if (args.extra && args.extra.search) {
+    // no need to cache search results
+    return kitsu.search(args.extra.search)
+        .then((metas) => ({ metas: metas, cacheMaxAge: CACHE_MAX_AGE }));
+  }
 
-	const options = {
-		offset: skip,
-		genre: args.extra && args.extra.genre,
-		sort: sortValue[args.id],
-		trending: args.id === 'kitsu-anime-trending'
-	};
+  const options = {
+    offset: skip,
+    genre: args.extra && args.extra.genre,
+    sort: sortValue[args.id],
+    trending: args.id === 'kitsu-anime-trending'
+  };
 
-	return cacheWrapCatalog(id, () => kitsu.animeEntries(options)
-			.then((metas) => ({ metas: metas, cacheMaxAge: CACHE_MAX_AGE })));
+  return cacheWrapCatalog(id, () => kitsu.animeEntries(options)
+      .then((metas) => ({ metas: metas, cacheMaxAge: CACHE_MAX_AGE })));
 });
 
 builder.defineMetaHandler((args) => {
-	if (args.id.match(/^kitsu:\d+$/)) {
-		const id = parseInt(args.id.replace('kitsu:', ''));
+  if (args.id.match(/^kitsu:\d+$/)) {
+    const id = parseInt(args.id.replace('kitsu:', ''));
 
-		return cacheWrapMeta(id, () => kitsu.animeData(id)
-				.then((metadata) => enrichKitsuMetadata(metadata, cinemeta.getCinemetaMetadata))
-				.then((meta) => ({ meta: meta, cacheMaxAge: CACHE_MAX_AGE })));
-	}
-	if (args.id.match(/^tt\d+$/)) {
-		const id = args.id;
+    return cacheWrapMeta(id, () => kitsu.animeData(id)
+        .then((metadata) => enrichKitsuMetadata(metadata, cinemeta.getCinemetaMetadata))
+        .then((meta) => ({ meta: meta, cacheMaxAge: CACHE_MAX_AGE })));
+  }
+  if (args.id.match(/^tt\d+$/)) {
+    const id = args.id;
 
-		if (!hasImdbMapping(id)) {
-			return Promise.reject(`No imdb mapping for: ${id}`);
-		}
+    if (!hasImdbMapping(id)) {
+      return Promise.reject(`No imdb mapping for: ${id}`);
+    }
 
-		return cacheWrapMeta(id, () => cinemeta.getCinemetaMetadata(id, args.type)
-				.then((metadata) => enrichImdbMetadata(metadata, kitsu.animeData))
-				.then((meta) => ({ meta: meta, cacheMaxAge: CACHE_MAX_AGE })));
-	}
+    return cacheWrapMeta(id, () => cinemeta.getCinemetaMetadata(id, args.type)
+        .then((metadata) => enrichImdbMetadata({ imdb_id: id, ...metadata }, kitsu.animeData))
+        .then((meta) => ({ meta: meta, cacheMaxAge: CACHE_MAX_AGE })));
+  }
 
-	return Promise.reject(new Error('invalid id'));
+  return Promise.reject(new Error('invalid id'));
 });
 
 module.exports = builder.getInterface();
