012d32ea31a26d6dea4d7006d61a602a6d2ff794
TheBeastLT <pauliox@beyond.lt>
2023-04-10 20:21:45 +0200

    update imdb mapping

--- mapping/mal_seasonal_import.js
+++ mapping/mal_seasonal_import.js
@@ -160,4 +160,4 @@ async function sequence(promises) {
       promise.then(result => func().then(Array.prototype.concat.bind(result))), Promise.resolve([]));
 }
 
-importMalSeason('2023/winter').then(season => `Finished importing MAL ${season}`);
+importMalSeason('2023/spring').then(season => `Finished importing MAL ${season}`);
